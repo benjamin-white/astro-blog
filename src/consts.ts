8650e7c78ccc373027a66ecddf08503c4e1e9d84
Ben White <benjaminwhite@live.co.uk>
2023-08-30 15:09:24 +0100

    feat: add blog content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Just Enough Stuff";
+export const SITE_DESCRIPTION = "A little blog about stuff.";
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,71 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+const css = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const typescript = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+});
+
+const stuff = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { css, typescript, stuff };
+
+export const TAXONOMIES = [
+  {
+    name: "CSS",
+    slug: "css",
+    description: "Stuff about CSS",
+  },
+  {
+    name: "Typescript",
+    slug: "typescript",
+    description: "Stuff about Typescript",
+  },
+  {
+    name: "Stuff",
+    slug: "stuff",
+    description: "Stuff about stuff",
+  },
+];
